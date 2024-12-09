d5209393d072aa20459d9f143685303d775ffc3f
Damaceno25 <damaceno2590@gmail.com>
2024-12-09 20:21:57 -0300

    Adicição de Uma pagína do card

--- src/app/conteudo/perfil/perfil.page.ts
+++ src/app/conteudo/perfil/perfil.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AutenticacaoService } from "../../services/autenticacao.service";
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
-export class PerfilPage implements OnInit {
+export class PerfilPage  {
 
-  constructor() { }
+  constructor(
+    private autenticacaoService : AutenticacaoService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  // Método para realizar o logout
+  logout() {
+    this.autenticacaoService.limparSessao(); // Limpa a sessão
+    this.router.navigate(['/login']); // Navega para a página de login
   }
 
+  
 }
